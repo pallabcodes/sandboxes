9353f241b12ae05bf121582f70ae3b547939ca07
pallabcodes <kayydraws@gmail.com>
2024-07-26 16:28:11 +0530

    SANDBOXES

--- src/controllers/userController.js
+++
@@ -1,7 +0,0 @@
-// Controller for user-related requests
-const userService = require('../services/userService');
-
-exports.getUsers = async (req, res) => {
-  const users = await userService.getAllUsers();
-  res.json(users);
-};
