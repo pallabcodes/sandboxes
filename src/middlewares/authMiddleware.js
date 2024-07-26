99a751b872f328526da2ae151f369be3587a36ca
pallabcodes <kayydraws@gmail.com>
2024-07-26 16:28:11 +0530

    SANDBOXES

--- src/middlewares/authMiddleware.js
+++
@@ -1,7 +0,0 @@
-// Middleware for authentication
-module.exports = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.status(401).send('Unauthorized');
-};
--- src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-// Routes related to user operations
-const express = require('express');
-const userController = require('../controllers/userController');
-
-const router = express.Router();
-
-router.get('/', userController.getUsers);
-
-module.exports = router;
